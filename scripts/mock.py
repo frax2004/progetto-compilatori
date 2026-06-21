e7830ea58b5ea4a217ee1880463e61218b95df1c
Francesco Pollarà <40788754+frax2004@users.noreply.github.com>
2026-06-21 21:54:22 +0200

    mock data

--- scripts/mock.py
+++ scripts/mock.py
@@ -1,95 +1,118 @@
 import random as rd
+from dataclasses import dataclass
 
+@dataclass
+class Coords:
+  x: float
+  y: float
 
-CITIES = tuple({
-  'PMO': 'Palermo',
-  'CFU': 'Cefalu',
-  'MSN': 'Messina',
-  "TRP": "Trapani",
-  "RMA": "Roma",
-  "FRZ": "Firenze",
-  "MLN": "Milano",
-  "TRN": "Torino",
-  "BAR": "Bari",
-  "CTN": "Catania",
-  "VRS": "Varese",
-  "VNZ": "Venezia",
-  "CTZ": "Catanzaro",
-  "GNV": "Genova"
-}.items())
+  @staticmethod
+  def random():
+    return Coords(rd.random() * 1_000, rd.random() * 1_000)
+
+  def __str__(self):
+    return f'({self.x}, {self.y})'
+
+@dataclass
+class City:
+  code: str
+  name: str
+
+  def __str__(self):
+    return f'{self.code} "{self.name}"'
+
+@dataclass
+class CityDecl:
+  city: City
+  coords: Coords
+
+  def __str__(self):
+    return f'{str(self.city)} {str(self.coords)}'
+
+@dataclass
+class CyclistDecl:
+  code: int
+  name: str
+  city: str
+
+  def __str__(self):
+    return f'{self.code}\n{self.name}\n{self.city}'
+
+@dataclass
+class StageStmt:
+  code: int
+  city: str
+  seconds: int
+
+  def __str__(self):
+    return f'{self.code} {self.city} {self.seconds}'
+
+CITIES = [
+  City('PMO', 'Palermo'),
+  City('CFU', 'Cefalu'),
+  City('MSN', 'Messina'),
+  City("TRP", "Trapani"),
+  City("RMA", "Roma"),
+  City("FRZ", "Firenze"),
+  City("MLN", "Milano"),
+  City("TRN", "Torino"),
+  City("BAR", "Bari"),
+  City("CTN", "Catania"),
+  City("VRS", "Varese"),
+  City("VNZ", "Venezia"),
+  City("CTZ", "Catanzaro"),
+  City("GNV", "Genova")
+]
+
+NUMBERS = [*range(1, 1001)]
 
 CYCLIST_SEP = '&&&'
 SECTION_SEP = '%%%'
-COORDINATES = tuple((rd.random() * 1_000, rd.random() * 1_000) for _ in range(2048))
-NUMBERS = tuple(range(1, 1001))
-SECONDS = tuple(rd.randint(10_000, 100_000) for _ in range(2048))
 
 FIRST_NAMES = "Gianmarco Marta Francesco Maria Riccardo Sara Emilio Virginia Dennis Vanessa Bjarne Tony".split(' ')
 LAST_NAMES = "Tocco Gilli Marzano Perrotti Pescini Colombo Evan Ritchie Kirby Stroustrap Stark".split(' ')
+SECONDS = tuple(rd.randint(10_000, 100_000) for _ in range(2048))
+
+def random_name():
+  return f'{rd.choice(FIRST_NAMES)} {rd.choice(LAST_NAMES)}'
+
 
 def clamp(min, x, max): return min if x < min else max if x > max else x
-  
+
 def random_subset(collection, count):
-  count = clamp(0, count, len(collection))
-  start = rd.randint(0, len(collection)-count)
-  end = start + count
-  return collection[start:end]
+  copy = [*collection]
+  rd.shuffle(copy)
+  return copy[0:clamp(0, count, len(copy)-1)]
 
 
 def happy_path(k_cities, k_cyclists, max_stages):
-  PLACES = list(zip(random_subset(CITIES, k_cities), rd.choices(COORDINATES, k = k_cities)))
+  POSSIBLE_CITIES = random_subset(CITIES, k_cities)
+  POSSIBLE_NUMBERS = random_subset(NUMBERS, k_cyclists)
+  
+  SECTION1 = {city.code: CityDecl(city, Coords.random()) for city in POSSIBLE_CITIES}
+  SECTION2 = {code: CyclistDecl(code, random_name(), rd.choice([*SECTION1.keys()])) for code in POSSIBLE_NUMBERS}
 
-  POSSIBLE_NUMBERS = list(random_subset(NUMBERS, k_cyclists))
-  rd.shuffle(POSSIBLE_NUMBERS)
 
-  CYCLISTS = tuple((
-    str(POSSIBLE_NUMBERS[i]),
-    f"{rd.choice(FIRST_NAMES)} {rd.choice(LAST_NAMES)}",
-    rd.choice(PLACES)[0][0]
-  ) for i in range(k_cyclists))
+  SECTION3 = []
+  possible_checkpoints = [*SECTION1.keys()]
+  current_states = {cyclist.code: cyclist.city for cyclist in SECTION2.values()}
+  for _ in range(max_stages):
+    code = rd.choice([*SECTION2.keys()])
+    prob = 1.0/(len(possible_checkpoints)-1)
+    weights = [prob if c != current_states[code] else 0 for c in possible_checkpoints]
+    city = rd.choices(possible_checkpoints, weights)[0]
+    SECTION3.append(StageStmt(code, city, rd.choice(SECONDS)))
+    current_states[code] = city
 
-  TIMINGS = tuple(
-    f"{rd.choice(CYCLISTS)[0]} {rd.choice(PLACES)[0][0]} {rd.choice(SECONDS)}" 
-    for _ in range(max_stages)
-  )
+  section1 = '\n'.join(map(str, SECTION1.values()))
+  section2 = f'\n{CYCLIST_SEP}\n'.join(map(str, SECTION2.values()))
+  section3 = '\n'.join(map(str, SECTION3))
 
-  SECTION1 = "\n".join("{} \"{}\" ({}, {})".format(*city, *coords) for (city, coords) in PLACES)
-  SECTION2 = f'\n{CYCLIST_SEP}\n'.join(
-    map(lambda cyclist: "\n".join(cyclist), CYCLISTS)
-  )
-  SECTION3 = '\n'.join(TIMINGS)
-
-  return f"\n{SECTION_SEP}\n".join([
-    SECTION1, 
-    SECTION2, 
-    SECTION3
-  ])
-
-def semantic_error(k_cities, k_cyclists, max_stages):
-  PLACES = list(zip(rd.choices(CITIES, k = k_cities), rd.choices(COORDINATES, k = k_cities)))
-
-  CYCLISTS = tuple((
-    str(rd.choice(NUMBERS)),
-    f"{rd.choice(FIRST_NAMES)} {rd.choice(LAST_NAMES)}",
-    rd.choice(CITIES)[0]
-  ) for _ in range(k_cyclists))
-
-  TIMINGS = tuple(
-    f"{rd.choice(NUMBERS)} {rd.choice(CITIES)[0]} {rd.choice(SECONDS)}" 
-    for _ in range(max_stages)
-  )
-
-  SECTION1 = "\n".join("{} \"{}\" ({}, {})".format(*city, *coords) for (city, coords) in PLACES)
-  SECTION2 = f'\n{CYCLIST_SEP}\n'.join(
-    map(lambda cyclist: "\n".join(cyclist), CYCLISTS)
-  )
-  SECTION3 = '\n'.join(TIMINGS)
-
-  return f"\n{SECTION_SEP}\n".join([
-    SECTION1, 
-    SECTION2, 
-    SECTION3
-  ])
+  return f"""{section1}
+{SECTION_SEP}
+{section2}
+{SECTION_SEP}
+{section3}"""
 
 
 gen_table = [
@@ -97,10 +120,6 @@ gen_table = [
   (happy_path, 6, 3, 30),
   (happy_path, 3, 5, 4),
   (happy_path, 2, 2, 9),
-  (semantic_error, 4, 7, 15),
-  (semantic_error, 6, 3, 30),
-  (semantic_error, 3, 5, 4),
-  (semantic_error, 2, 2, 9)
 ]
 
 import subprocess
